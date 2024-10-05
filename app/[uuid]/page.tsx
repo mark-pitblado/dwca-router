929ad4d42366dc185f74c9763eee8624f38f26ae
Mark Pitblado <mark@pitblado.me>
2024-10-05 14:08:59 -0700

    add qr code generator

--- app/[uuid]/page.tsx
+++ app/[uuid]/page.tsx
@@ -1,9 +1,17 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+import { config } from "@/config.ts";
+
+import Link from "next/link";
+
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -18,17 +26,35 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
-
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter,
+} from "@/components/ui/dialog";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
 export default function UuidPage({ params }: { params: { uuid: string } }) {
   const { uuid } = params;
   const router = useRouter();
   const [domain, setDomain] = useState("");
+  const [qrCodeUrl, setQrCodeUrl] = useState<string | null>(null);
+  const [resolverDomain, setResolverDomain] = useState<string>("");
+  const [size, setSize] = useState<string>("150");
+  const [format, setFormat] = useState<string>("png");
+  const [errorCorrection, setErrorCorrection] = useState<string>("M");
+
+  useEffect(() => {
+    setResolverDomain(config.resolverDomain);
+  }, []);
 
   const handlePlaintextClick = () => {
     router.push(`/plaintext/${uuid}`);
@@ -89,6 +115,12 @@ export default function UuidPage({ params }: { params: { uuid: string } }) {
     router.push(`/images/${uuid}`);
   };
 
+  const handleGenerateQrCode = () => {
+    const url = `https://${resolverDomain}/${uuid}`;
+    const qrCodeApiUrl = `https://quickchart.io/qr?text=${encodeURIComponent(url)}&size=${encodeURIComponent(size)}&format=${encodeURIComponent(format)}&ecLevel=${errorCorrection}`;
+    window.location.href = qrCodeApiUrl;
+  };
+
   const handleBadgeClick = () => {
     navigator.clipboard.writeText(uuid).then(
       () => {
@@ -110,10 +142,117 @@ export default function UuidPage({ params }: { params: { uuid: string } }) {
             </BreadcrumbItem>
             <BreadcrumbSeparator />
             <BreadcrumbItem>
-              <BreadcrumbPage>Details</BreadcrumbPage>
+              <BreadcrumbPage>Options</BreadcrumbPage>
             </BreadcrumbItem>
           </BreadcrumbList>
         </Breadcrumb>{" "}
+        <Dialog>
+          <DialogTrigger asChild>
+            <div className="cursor-pointer mt-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="2em"
+                height="2em"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M3 11V3h8v8zm2-2h4V5H5zM3 21v-8h8v8zm2-2h4v-4H5zm8-8V3h8v8zm2-2h4V5h-4zm4 12v-2h2v2zm-6-6v-2h2v2zm2 2v-2h2v2zm-2 2v-2h2v2zm2 2v-2h2v2zm2-2v-2h2v2zm0-4v-2h2v2zm2 2v-2h2v2z"
+                />
+              </svg>
+            </div>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[600px]">
+            <DialogHeader>
+              <DialogTitle>Generate QR Code</DialogTitle>
+              <DialogDescription>
+                Generate a QR code that links to this resolver
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" className="text-right">
+                  Resolver root
+                </Label>
+                <Input
+                  id="resolverDomain"
+                  type="url"
+                  onChange={(e) => setResolverDomain(e.target.value)}
+                  value={resolverDomain}
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="size" className="text-right">
+                  Size (pixels)
+                </Label>
+                <Input
+                  id="size"
+                  type="number"
+                  value={size}
+                  onChange={(e) => setSize(e.target.value)}
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="format" className="text-right">
+                  Format
+                </Label>
+                <RadioGroup
+                  value={format}
+                  onValueChange={(value) => setFormat(value)}
+                  defaultValue="png"
+                  className="col-span-3"
+                >
+                  <div className="flex items-center space-x-4">
+                    <div className="flex items-center space-x-2">
+                      <RadioGroupItem value="png" id="png" />
+                      <Label htmlFor="png">PNG</Label>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                      <RadioGroupItem value="svg" id="svg" />
+                      <Label htmlFor="svg">SVG</Label>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                      <RadioGroupItem value="base64" id="base64" />
+                      <Label htmlFor="base64">Base 64</Label>
+                    </div>
+                  </div>
+                </RadioGroup>
+              </div>
+
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="errorCorrection" className="text-right">
+                  Error correction
+                </Label>
+                <RadioGroup
+                  defaultValue="M"
+                  value={errorCorrection}
+                  onValueChange={(value) => setErrorCorrection(value)}
+                  className="col-span-3"
+                >
+                  <div className="flex items-center space-x-4">
+                    <div className="flex items-center space-x-2">
+                      <RadioGroupItem value="L" id="low" />
+                      <Label htmlFor="low">Low</Label>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                      <RadioGroupItem value="M" id="medium" />
+                      <Label htmlFor="medium">Medium</Label>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                      <RadioGroupItem value="H" id="high" />
+                      <Label htmlFor="high">High</Label>
+                    </div>
+                  </div>
+                </RadioGroup>
+              </div>
+            </div>
+            <DialogFooter>
+              <Button onClick={handleGenerateQrCode}>Generate</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
         <TooltipProvider>
           <Tooltip>
             <TooltipTrigger>
@@ -206,6 +345,13 @@ export default function UuidPage({ params }: { params: { uuid: string } }) {
           >
             Images
           </Button>
+
+          <Link
+            className="pt-16 underline text-sm text-gray-500 cursor-pointer"
+            href={`/faq/${uuid}`}
+          >
+            Help
+          </Link>
         </div>
       </div>
     </div>
---
+++ config.ts
@@ -0,0 +1,3 @@
+export const config = {
+  resolverDomain: "https://dwca.net",
+};
