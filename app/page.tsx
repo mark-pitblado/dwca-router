78582aed2d8224fb82bacfeb952bbd11eff7c2c9
Mark Pitblado <mark@pitblado.me>
2024-10-05 14:09:21 -0700

    add default uuid value on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default function ResolverPage() {
   const [inputValue, setInputValue] = useState("");
   const [error, setError] = useState("");
   const router = useRouter();
@@ -16,12 +16,16 @@ export default function Home() {
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    if (!uuidRegex.test(inputValue)) {
+    const finalInputValue =
+      inputValue.trim() === ""
+        ? "4c906ce8-a2bf-425f-8d50-82197e918028"
+        : inputValue;
+    if (!uuidRegex.test(finalInputValue)) {
       setError("Invalid UUID (version 1 or 4)");
     } else {
       setError("");
       // Navigate to /{uuid} route with the valid UUID
-      router.push(`/${inputValue}`);
+      router.push(`/${finalInputValue}`);
     }
   };
 
@@ -29,6 +33,11 @@ export default function Home() {
     <div className="flex h-screen justify-center items-center bg-[#f5f5f5]">
       <div className="flex flex-col items-center space-y-4 w-full max-w-sm">
         <h1 className="text-2xl font-bold">Resolver</h1>
+        <p className="text-gray-500">
+          Enter an occurrenceID for a specimen shared in Darwin Core. Must
+          follow the UUID v1 or v4 standard. Click Submit to use the example
+          value.
+        </p>
         <form
           className="flex w-full flex-col items-center space-y-2"
           onSubmit={handleSubmit}
@@ -36,7 +45,7 @@ export default function Home() {
           <div className="flex w-3/4 md:w-full items-center space-x-2">
             <Input
               type="text"
-              placeholder="UUID"
+              placeholder="4c906ce8-a2bf-425f-8d50-82197e918028"
               value={inputValue}
               onChange={(e) => setInputValue(e.target.value)}
             />
