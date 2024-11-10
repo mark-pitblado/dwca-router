13a0d25d47c8f059df4d2ece8025f285b5d8483e
Mark Pitblado <mark@pitblado.me>
2024-11-10 09:02:01 -0800

    fix: import of vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
